f8daa96a57bc26c381e09895bc3f4ab04b6a671c
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2022-09-27 20:49:26 -0300

    feat: Configurando S3 e ajustando materiais

--- controllers/materiais.js
+++ controllers/materiais.js
@@ -1,33 +1,31 @@
 const mongoose = require("mongoose");
 const MateriaisInfo = require("../models/materiaisInfo");
+const db = require("../database/db");
 
 exports.getMateriaisByTurma = async (req, res) => {
    const { id } = req.params;
 
    try {
-      MateriaisInfo.find({'turma': id}, (err, materiais) => { 
-         if(err) throw Error(err);
+      const materiais = await db.exec("SELECT * FROM Materiais WHERE turmaId = ?", id);
 
-         res.status(200).json(materiais);
-      });
+      res.status(200).json(materiais);
    } catch (error) {
       res.status(500).json(error);
    }
 }
 
 exports.addMateriais = async (req, res) => {
+   const { originalname: name, size, key, location: url = "" } = req.file;
    const { id } = req.params;
-   const { nome, link, userId } = req.body;
+   const { userId } = req.body;
 
    try {
-      MateriaisInfo.create({nome, link, author: userId, turma: id }, (err, material) => {
-         if(err) throw Error(err);
+   const newMaterial = await db.exec("INSERT INTO Materiais (nome, link, authorId, turmaId, fileSize) VALUES (?)", 
+      [[name, url, userId, id, size]]);
 
-         res.status(201).json(material);
-      });
-
+   res.status(201).json(newMaterial);
    } catch (error) {
-      res.status(409).json(error);
+      res.status(409).json(error.message);
    }
 }
 
@@ -35,11 +33,8 @@ exports.removeMateriais = async (req, res) => {
    const { id } = req.params;
 
    try {
-      await MateriaisInfo.findByIdAndRemove(id).then(() => {
-         if(err) throw Error(err);
-   
-         res.status(200).json({ message: "Material deletado com successo"})
-      });
+      await db.exec("DELETE FROM Materiais WHERE id = ?", id);
+      res.status(200).json({ message: "Material deletado com successo"})
    } catch (error) {
       res.status(404).json(error);
    }
